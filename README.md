1a16602eb971a7a1d288ca2f56b0cb5906791006
pawantiwari1 <tiwaripawan388@gmail.com>
2022-06-28 17:50:26 +0530

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # angular-file-upload-ysg9bw
 
+# Upload file with multiple format
+
+# like single selection
+
+# multiple selection
+
+# upload percentage
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-file-upload-ysg9bw)
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component, VERSION } from '@angular/core';
-import {HttpClientModule, HttpClient, HttpRequest, HttpResponse, HttpEventType} from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpClient,
+  HttpRequest,
+  HttpResponse,
+  HttpEventType,
+} from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
   template: `
-    Version = {{version.full}} <br/>
+     <br/>
     <input type="file" (change)="upload($event.target.files)" />
     Upload Percent: {{percentDone}}% <br />
     
@@ -17,60 +23,64 @@ export class AppComponent {
   percentDone: number;
   uploadSuccess: boolean;
 
-  constructor(
-    private http: HttpClient,
-    ) { }
-    
-  version = VERSION
-  
-  upload(files: File[]){
+  constructor(private http: HttpClient) {}
+
+  version = VERSION;
+
+  upload(files: File[]) {
     //pick from one of the 4 styles of file uploads below
     this.uploadAndProgress(files);
   }
 
-  basicUpload(files: File[]){
+  basicUpload(files: File[]) {
     var formData = new FormData();
-    Array.from(files).forEach(f => formData.append('file', f))
-    this.http.post('https://file.io', formData)
-      .subscribe(event => {  
-        console.log('done')
-      })
+    Array.from(files).forEach((f) => formData.append('file', f));
+    this.http.post('https://file.io', formData).subscribe((event) => {
+      console.log('done');
+    });
   }
-  
+
   //this will fail since file.io dosen't accept this type of upload
   //but it is still possible to upload a file with this style
-  basicUploadSingle(file: File){    
-    this.http.post('https://file.io', file)
-      .subscribe(event => {  
-        console.log('done')
-      })
+  basicUploadSingle(file: File) {
+    this.http.post('https://file.io', file).subscribe((event) => {
+      console.log('done');
+    });
   }
-  
-  uploadAndProgress(files: File[]){
-    console.log(files)
+
+  uploadAndProgress(files: File[]) {
+    console.log(files);
     var formData = new FormData();
-    Array.from(files).forEach(f => formData.append('file',f))
-    
-    this.http.post('https://file.io', formData, {reportProgress: true, observe: 'events'})
-      .subscribe(event => {
+    Array.from(files).forEach((f) => formData.append('file', f));
+
+    this.http
+      .post('https://file.io', formData, {
+        reportProgress: true,
+        observe: 'events',
+      })
+      .subscribe((event) => {
         if (event.type === HttpEventType.UploadProgress) {
-          this.percentDone = Math.round(100 * event.loaded / event.total);
+          this.percentDone = Math.round((100 * event.loaded) / event.total);
         } else if (event instanceof HttpResponse) {
           this.uploadSuccess = true;
         }
-    });
+      });
   }
-  
+
   //this will fail since file.io dosen't accept this type of upload
   //but it is still possible to upload a file with this style
-  uploadAndProgressSingle(file: File){    
-    this.http.post('https://file.io', file, {reportProgress: true, observe: 'events'})
-      .subscribe(event => {
+  uploadAndProgressSingle(file: File) {
+    this.http
+      .post('https://file.io', file, {
+        reportProgress: true,
+        observe: 'events',
+      })
+      .subscribe((event) => {
         if (event.type === HttpEventType.UploadProgress) {
-          this.percentDone = Math.round(100 * event.loaded / event.total);
+          this.percentDone = Math.round((100 * event.loaded) / event.total);
         } else if (event instanceof HttpResponse) {
           this.uploadSuccess = true;
         }
-    });
+      });
   }
 }
